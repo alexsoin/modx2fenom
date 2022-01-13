daad58784198c62ad999394a24cd248d0f4948a2
Alex Soin <3787132+alexsoin@users.noreply.github.com>
2022-01-13 09:28:05 +0400

    add: lexicon

--- src/m2f.list.js
+++ src/m2f.list.js
@@ -3,6 +3,7 @@ export default [
 	{ name: "placeholder", tokenM: "+" },
 	{ name: "url", tokenM: "~" },
 	{ name: "chunk", tokenM: "$" },
+	{ name: "lexicon", tokenM: "%" },
 	{ name: "snippet", tokenM: "!", preName: "!" },
 	{ name: "snippet", default: true },
 ];
